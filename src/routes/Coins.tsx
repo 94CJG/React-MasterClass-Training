15ef84aaa5030dfd8a689b66f3e83d1d92dadf2f
94CJG <krsoi9412@gmail.com>
2023-07-16 23:27:07 +0900

    Coins.tsx 프론트 페이지 CSS 수정
    ㅁ map( ) 함수를 이용하여 styled Component를 수정
    ㅁ Link to + map ( )를 이용하여 클릭시 해당 페이지로 이동함.

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,11 +1,88 @@
-//	-> All coins 모든 코인들이 보여질 것이다.
-//	:id -> /btc -> Coin Detail
-//	Nested(중첩된) Router 사용 할 것이다
-//	ㄴ what? 한 스크린 내에 또다른 Router를 가질수 있는 것이다.
-//	ㄴ btc/information
-//	ㄴ btc/chart
 
-function Coins () {
-	return <h1>Coins</h1>
+import {Link} from "react-router-dom";
+import { styled } from 'styled-components';
+
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  padding: 20px; 
+  border-radius: 15px;
+  margin-bottom: 10px;
+	a {
+		transition: color 0.2s ease-in;
+		display: block;
+		//a 링크에 padding이 있을시 유저에게 더 편하게 다가올 수 있다.
+		//마우스를 올렸을 때 보여진다.
+	}
+	&:hover {
+		a {
+			color: ${(props) => props.theme.accentColor };
+		}
+	}
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const coins = [
+	{
+		id: 'btc-bitcoin',
+		name: 'Bitcoin',
+		symbol: 'BTC',
+		rank: 1,
+		is_new: false,
+		is_active: true,
+		type: 'coin',
+	},
+	{
+		id: 'eth-ethereum',
+		name: 'Ethereum',
+		symbol: 'ETH',
+		rank: 2,
+		is_new: false,
+		is_active: true,
+		type: 'coin',
+	},
+	{
+		id: 'hex-hex',
+		name: 'HEX',
+		symbol: 'HEX',
+		rank: 3,
+		is_new: false,
+		is_active: true,
+		type: 'token',
+	},
+];
+
+function Coins() {
+	return (
+		<Container>
+			<Header>
+				<Title>코인</Title>
+			</Header>
+			<CoinsList>
+				{coins.map((coin) => (
+					<Coin key={coin.id}>
+						<Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+					</Coin>
+				))}
+			</CoinsList>
+		</Container>
+	);
 }
 export default Coins;
--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
-import {DefaultTheme} from "styled-components/dist/types";
+import {DefaultTheme} from "styled-components";
 
-export const theme:DefaultTheme = { //styled.d.ts에서 정의를 해줘서 자동완성으로 보여진다.
-	bgColor:"white",
-	textColor:"black",
-	btnColor:"tomato",
-}
+export const theme: DefaultTheme = { //styled.d.ts에서 정의를 해줘서 자동완성으로 보여진다.
+	bgColor:"#2f3640",
+	textColor:"#f5f6fa",
+	accentColor:"#9c88ff",
+};
