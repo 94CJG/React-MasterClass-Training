bf92dd00de64f706e5bb7f1d3f5c2cbd72b2ce68
94CJG <krsoi9412@gmail.com>
2023-07-21 00:28:24 +0900

    route 안에 route를 불러온다.
    ㅁ 169번째줄 코드를 이용하여 저 경로 설정을 해줄수 있다.
    ㅁ  * Nested router 혹은 nested route는 route안에 있는 또 다른 route 라고 한다.
     	 ㄴ 웹사이트에서 탭을 사용 할 때 우릴 많이 도와줄 것이다.

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,13 +1,8 @@
-
 import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { Switch, Route, useLocation, useParams } from "react-router";
 import styled from "styled-components";
-
-const Container = styled.div`
-  padding: 0px 20px;
-	max-width: 480px;
-	margin: 0 auto;
-`;
+import Chart from "./Chart";
+import Price from "./Price";
 
 const Title = styled.h1`
   font-size: 48px;
@@ -15,39 +10,72 @@ const Title = styled.h1`
 `;
 
 const Loader = styled.span`
-	text-align: center;
-	display: block;
+  text-align: center;
+  display: block;
+`;
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
 `;
 
 const Header = styled.header`
-  height: 10vh;
+  height: 15vh;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-interface RouteParmas {
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
+interface RouteParams {
 	coinId: string;
 }
-
 interface RouteState {
 	name: string;
 }
-
 interface InfoData {
 	id: string;
 	name: string;
 	symbol: string;
 	rank: number;
-	circulating_supply: number;
-	total_supply: number;
-	max_supply: number;
-	beta_value: number;
+	is_new: boolean;
+	is_active: boolean;
+	type: string;
+	description: string;
+	message: string;
+	open_source: boolean;
+	started_at: string;
+	development_status: string;
+	hardware_wallet: boolean;
+	proof_type: string;
+	org_structure: string;
+	hash_algorithm: string;
 	first_data_at: string;
-	last_updated: string;
-	quotes: object;
+	last_data_at: string;
 }
-
 interface PriceData {
 	id: string;
 	name: string;
@@ -70,7 +98,7 @@ interface PriceData {
 			percent_change_6h: number;
 			percent_change_7d: number;
 			percent_change_12h: number;
-			percent_change_15m: number
+			percent_change_15m: number;
 			percent_change_24h: number;
 			percent_change_30d: number;
 			percent_change_30m: number;
@@ -79,45 +107,86 @@ interface PriceData {
 			volume_24h: number;
 			volume_24h_change_24h: number;
 		};
-	}
-};
+	};
+}
 
 function Coin() {
 	const [loading, setLoading] = useState(true);
-	const { coinId } = useParams<RouteParmas>();
+	const { coinId } = useParams<RouteParams>();
 	const { state } = useLocation<RouteState>();
 	const [info, setInfo] = useState<InfoData>();
 	const [priceInfo, setPriceInfo] = useState<PriceData>();
-	//console.log(state); // 타이틀에 coin의 name을 직접 뿌려줄 수 있다는 의미이다.
-	//console.log(coinId);
 	useEffect(() => {
 		(async () => {
-			const infoData = await ((await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json()); //API 받아오는 코드를 캡슐화된 코드
-			//console.log(infoData)
-			const priceData = await ((await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json());
-			//console.log(priceData)
-			setInfo(infoData); // 타입스크립트는 빈 객체로 인식해서 화면에는 아무것도 보여지지 않는다.
+			const infoData = await (
+				await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+			).json();
+			const priceData = await (
+				await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+			).json();
+			setInfo(infoData);
 			setPriceInfo(priceData);
+			setLoading(false);
 		})();
-	}, [])
+	}, [coinId]);
 	return (
 		<Container>
 			<Header>
-				<Title>{state?.name || "Loading.."} </Title>
+				<Title>
+					{state?.name ? state.name : loading ? "Loading..." : info?.name}
+				</Title>
 			</Header>
-			{loading ? (<Loader>Loading...</Loader>) : null}
+			{loading ? (
+				<Loader>Loading...</Loader>
+			) : (
+				<>
+					<Overview>
+						<OverviewItem>
+							<span>Rank:</span>
+							<span>{info?.rank}</span>
+						</OverviewItem>
+						<OverviewItem>
+							<span>Symbol:</span>
+							<span>${info?.symbol}</span>
+						</OverviewItem>
+						<OverviewItem>
+							<span>Open Source:</span>
+							<span>{info?.open_source ? "Yes" : "No"}</span>
+						</OverviewItem>
+					</Overview>
+					<Description>{info?.description}</Description>
+					<Overview>
+						<OverviewItem>
+							<span>Total Suply:</span>
+							<span>{priceInfo?.total_supply}</span>
+						</OverviewItem>
+						<OverviewItem>
+							<span>Max Supply:</span>
+							<span>{priceInfo?.max_supply}</span>
+						</OverviewItem>
+					</Overview>
+					<Switch>
+						<Route path={`/${coinId}/price`}>
+							<Price />
+						</Route>
+						<Route path={`/${coinId}/chart`}>
+							<Chart />
+						</Route>
+					</Switch>
+				</>
+			)}
 		</Container>
 	);
 }
 export default Coin;
 
 /**
- * 이번강의 내용 요약
- * console.log()를 이용하여 API내용 확인.
- * API내용을 확인 후 Store object as global variable를 이용하여 해당 원하는 값을 가지고 온다.
- * Object.keys(temp1).map((k) => typeof k).join() 를 이용했다.
- * type Script에 해당하는 타입을 작성하였다.
- * useState() 타입을 지정.
- * 타이 스크립트에서 인터페이스 명을 지정해줄 때 관례로 맨 앞에 I를 붙이거나 or 안붙인다. 그건 개인 맘대로!
+ * Nested router 혹은 nested route는 route안에 있는 또 다른 route 라고 한다.
+ * ㄴ 웹사이트에서 탭을 사용 할 때 우릴 많이 도와줄 것이다.
  * 
+ * 
+ * 
+ * 이번강의 내용 요약
+ * CSS 컴포넌트는 소스를 가져옴
+ * route 안에 route를 만들 수 있다. 
  */
