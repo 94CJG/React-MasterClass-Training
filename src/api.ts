377322b69a8e5cfd6f3bfd15f5931765f4a61081
94CJG <krsoi9412@gmail.com>
2023-07-27 17:09:55 +0900

    API요청이 유료화 됐어서, 니코쌤 자체적 API로 수업 진행
    ㅁ Coin.tsx 라우트에서 props로 coinId를 전달함.
    ㅁ interface 설정

--- src/api.ts
+++ src/api.ts
@@ -12,6 +12,9 @@ export function fetchCoinTickers(coinId: string){
 	return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) => response.json());
 }
 
+export function fetchCoinHistory(coinId:string) {
+	return fetch(`${BASE_URL}/coins/${coinId}`).then((response) => response.json());
+}
 
 /**
  * 사용하기 위해서는 첫 단계로 fetcher함수를 만들어야 한다.
--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,5 +1,20 @@
-function Chart() {
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+
+interface ChartProps {
+	coinId: string;
+}
+
+function Chart({ coinId }: ChartProps) { //props 넘겨주는게 헷갈린다.
+	const {isLoading, data} = useQuery(["ohlcv", coinId], () => fetchCoinHistory(coinId));
 	return <h1>Chart</h1>
 }
 
 export default Chart;
+
+
+/**
+ * Chart component는 우리가 보고자 하는 가격의 암호화폐가 무엇인지
+ * 알아야 한다.
+ * 
+ */
