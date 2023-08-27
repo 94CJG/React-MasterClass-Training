a1c354eb87688f8f983c06834276051692c6891a
94CJG <krsoi9412@gmail.com>
2023-08-27 20:37:55 +0900

    6.7 Form Validation
    npm install react-hook-form
    
    ㅁ form을 사용하기 쉽게 하기 위한해 존재함.
    ㅁ register, watch 메서드가 존재한다.
    
    ㅁ register: name, onBlur, onChange, onClick, ref를 return 하는 함수
    - register( ) 함수가 반환하는 객체를 input의 props로 사용 가능하다. (40~45번째 줄 참고)
    
    ㅁ watch: form의 입력값들을 실시간으로 변화 모습들을 보여주는 함수

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-function ToDoList() {
+/*function ToDoList() {
 	const [toDo, setToDo] = useState("");
+	const [toDoError, setToDoError] = useState("");
 	const onChange = (event:React.FormEvent<HTMLInputElement>) => {
-		const { currentTarget: {value}, } = event; //
+		const { currentTarget: {value}, } = event;
+		setToDoError("");
 		setToDo(value);
 	};
 	const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
-		console.log(toDo);
+		if (toDo.length < 10) {
+			return setToDoError("To do should be longer");
+		}
+		console.log("submit");
 	};
 	
 	return (
@@ -17,12 +23,36 @@ function ToDoList() {
 			<form onSubmit={onSubmit}>
 				<input onChange={onChange} value={toDo} placeholder="Write a to do" />
 				<button>Add</button>
+				{toDoError !== "" ? toDoError : null}
+			</form>
+		</div>
+	);
+}*/
+
+function ToDoList() {
+	const { register, watch } = useForm(); //register 함수를 사용하면 onChange 이벤트 핸들러가 필요없다.
+	console.log(watch());
+	return (
+		<div>
+			<h1>ToDo-List Start</h1>
+			<form>
+				<input {...register("email")} placeholder="Email" />
+				<input {...register("firstName")} placeholder="First Name" />
+				<input {...register("lsatName")} placeholder="Last Name" />
+				<input {...register("username")} placeholder="User Name" />
+				<input {...register("password")} placeholder="PasswWord" />
+				<input {...register("password1")} placeholder="PassWord1" />
+				<button>Add</button>
 			</form>
 		</div>
 	);
 }
+
 export default ToDoList;
 
 /**
- *
+ * ㅁ JS 스프레드 연산자
+ * <input {...register("toDo")} placeholder="Write a to do" />
+ * 객체나 배열을 펼쳐서 해당 컨텍스트에 속한 개별 속성 또는 요소로 분리하는 역할을 한다.
+ * 
  */
